7edf62af527a2b67252fafd6a98607a0f79e91c2
RENAN <renan@redinateur.home>
2020-04-27 15:09:48 +0200

    test

--- views/NousContacter.php
+++ views/NousContacter.php
@@ -1,42 +1,55 @@
 <!doctype html>
 <html lang="fr">
-<head>
-	<meta http-equiv="content-type" content="text/html; charset=utf-8">
-  <title>Nous contacter</title>
-  <link rel="stylesheet" href="style.css">
-  <!--<script src="script.js"></script>-->
-</head>
-<body>
-<?php require_once "menu.php";?>
 
-	<div id="NousContacter">
-		<div id="titre">
-			Nous Contacter
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <title>Nous contacter</title>
+    <link rel="stylesheet" href="style.css">
+    <!--<script src="script.js"></script>-->
+</head>
+
+<body>
+    <?php require_once "menu.php"; ?>
+
+    <div id="NousContacter">
+        <div id="titre">
+            Nous Contacter
         </div>
         <div class="mail">
             <img src="mail.png" class="contacter_image" style="width: 140px; height: 100px;"></a>
-            <p><font size="+1">infinitemeasures.society@gmail.com</font></p>
+            <p>
+                <font size="+1">infinitemeasures.society@gmail.com</font>
+            </p>
         </div>
-        <div class="ReseauSociaux"> 
+        <div class="ReseauSociaux">
             <div id="contacter_block">
                 <a href="https://www.instagram.com/infinite_measures_society/"> <img src="logoInsta.png" class="contacter_image"></a>
-                <p><font size="+1">Instagram</font></p>
+                <p>
+                    <font size="+1">Instagram</font>
+                </p>
             </div>
             <div id="contacter_block">
-                <a href="https://twitter.com/InfiniteMeasur2" ><img src="logoTwitter.png" class="contacter_image"></a>
-                <p><font size="+1">Twitter</font></p>
+                <a href="https://twitter.com/InfiniteMeasur2"><img src="logoTwitter.png" class="contacter_image"></a>
+                <p>
+                    <font size="+1">Twitter</font>
+                </p>
             </div>
             <div id="contacter_block">
                 <a href="https://www.linkedin.com/in/infinite-measures-8454771a5/"><img src="logoLinked.png" class="contacter_image"></a>
-                <p><font size="+1">Linkedin</font></p>
+                <p>
+                    <font size="+1">Linkedin</font>
+                </p>
             </div>
             <div id="contacter_block">
-                <a href="https://www.facebook.com/Infinite-Measures-107016680954018/"><img src="logoFacebook.png" class="contacter_image" ></a>
-                <p><font size="+1">Facebook</font></p>
+                <a href="https://www.facebook.com/Infinite-Measures-107016680954018/"><img src="logoFacebook.png" class="contacter_image"></a>
+                <p>
+                    <font size="+1">Facebook</font>
+                </p>
             </div>
         </div>
-	</div>
-	<?php require_once "footer.php";?>
+    </div>
+    <?php require_once "footer.php"; ?>
 
 </body>
+
 </html>

1306cf27041354df2a71b69ce6f8516bc0c957e5
FranHmlr <58689891+FranHmlr@users.noreply.github.com>
2020-04-30 18:36:13 +0200

    Update menu.php

--- views/menu.php
+++ views/menu.php
@@ -18,7 +18,7 @@ if (isset($_SESSION['idUtilisateur'])) {
 	<a class="header2" href="Professionnel.php">Professionnels</a>
 	<a class="header2" href="NousContacter.php">Nous contacter</a>
 	<a class="header2" href="faq.php">FAQ</a>
-	<a class="header2" href="PageErreur.html"><img id="loupe" src="loupe4.png"></a>
+	<a class="header2" href="PageErreur.html"><img class="search" src="loupe4.png"></a>
 	<a class="header3" href="login.php"><?php echo $loginMsg; ?></a>
 	<a class="header2" href="logout.php"><?php echo $profilconnecte; ?></a>
 </header>

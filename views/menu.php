989692883f7af4f423b1022737af95400fcd1ea8
FranHmlr <58689891+FranHmlr@users.noreply.github.com>
2020-05-02 20:48:33 +0200

    Update menu.php

--- views/menu.php
+++ views/menu.php
@@ -12,14 +12,33 @@ if (isset($_SESSION['idUtilisateur'])) {
 }
 ?>
 
+<script>
+	function cacher(id) {
+		document.getElementById(id).setAttribute("display", "none");
+	}
+
+	function montrer(id) {
+		document.getElementById(id).setAttribute("display", "default");
+	}
+</script>
+
 <header>
 	<a href="main.php"><img id="logo" class="erreur_image" src="im.gif"></a>
 	<a class="header2" href="main.php">Accueil</a>
 	<a class="header2" href="Professionnel.php">Professionnels</a>
 	<a class="header2" href="NousContacter.php">Nous contacter</a>
 	<a class="header2" href="faq.php">FAQ</a>
-	<button class="loupe"> <img class="search" src="loupe4.png"> </button>
+	<button id="bouttonloupe" onclick="cacher('bouttonloupe'), montrer('recherche')"> 
+		<img class="loupe" src="loupe.png"> 
+	</button>
 	<a class="header3" href="login.php"><?php echo $loginMsg; ?></a>
 	<a class="header2" href="logout.php"><?php echo $profilconnecte; ?></a>
 </header>
+
+<form methode ="get" action="PageErreur.html" id="recherche">
+   	<p> <input type="search" id="texteRecherche" placeholder="Que recherchez-vous ?"/></p>
+</form>
+
 <button id="gotop"><a href="#top">&#8743</a></button>
+
+

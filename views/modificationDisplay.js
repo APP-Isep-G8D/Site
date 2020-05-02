10d6b24d432823a80fe6ab5a139ce19ba71dd9ce
FranHmlr <58689891+FranHmlr@users.noreply.github.com>
2020-05-02 21:53:39 +0200

    Add files via upload

---
+++ views/modificationDisplay.js
@@ -0,0 +1,14 @@
+
+
+
+function cacher(id) {
+		var objet=document.getElementById(id);
+		objet.setAttribute("background-color", "black");
+}
+
+
+
+function montrer(id) {
+	var objet = document.getElementById(id);
+	objet.setAttribute("display", "default");
+}

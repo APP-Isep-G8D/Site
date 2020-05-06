987781aefa62c95b3d7ce8fedc7c54703cd0e35c
RENAN <renan@redinateur.home>
2020-05-06 11:54:40 +0200

    Fonctionnel

--- views/envoieMail_phpmailer.php
+++ views/envoieMail_phpmailer.php
@@ -10,35 +10,30 @@ require '../PHPMailer/src/SMTP.php';
 
 $mail = new PHPMailer(true);
 
+$mail->setLanguage('fr', '../PHPMailer/language/');
+
 try {
     //Server settings
-    $mail->SMTPDebug = 4; // Enable verbose debug output
-    $mail->isMail(); // Send using SMTP
-    $mail->Host = gethostbyname('smtp.gmail.com'); // Set the SMTP server to send through
+
+    $mail->SMTPDebug = 4; // Niveau de debug
+    $mail->isSMTP(); // Utiliser SMTP
+    $mail->Host = 'smtp.gmail.com'; // Set the SMTP server to send through
     $mail->SMTPAuth = true; // Enable SMTP authentication
-    $mail->Username = ''; // SMTP username
-    $mail->Password = ''; // SMTP password
-    $mail->SMTPSecure = 'tls'; // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-    $mail->Port = 587; // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+    $mail->Username = 'infinitemeasures.society@gmail.com'; // SMTP username
+    $mail->Password = '1@Razertyuiop'; // SMTP password
+    $mail->SMTPSecure = 'ssl'; // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port = 465; // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
-    //Recipients
-    $mail->setFrom('chiffrer@mail.com', 'Envoyeur');
-    $mail->addAddress('infinitemeasures.society@gmail.com', 'Receveur'); // Add a recipient
-    /*
-    $mail->addReplyTo('info@example.com', 'Information');
-    $mail->addCC('cc@example.com');
-    $mail->addBCC('bcc@example.com');
-    */
+    //En-tete
+    $mail->setFrom('nouveaumail@mail.com', 'Envoyeur'); //Envoyeur
+    $mail->addAddress('infinitemeasures.society@gmail.com', 'Receveur'); // Receveur
+    $mail->addReplyTo('info@example.com', 'Information'); //Répondre à
 
-    // Attachments
-    //$mail->addAttachment('/var/tmp/file.tar.gz'); // Add attachments
-    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg'); // Optional name
-
-    // Content
-    $mail->isHTML(true); // Set email format to HTML
-    $mail->Subject = 'Encore un autre test';
-    $mail->Body = 'le texte est <b>en gras</b>';
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+    // Contenue
+    $mail->isHTML(true); // Format HTML
+    $mail->Subject = 'Encore un autre test'; //Objet
+    $mail->Body = 'le texte est <b>en gras</b>'; //Texte
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients'; //Texte si non HTML
 
     $mail->send();
     echo 'Message has been sent';

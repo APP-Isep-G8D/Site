e64e34309bd6327b26428f560fc211e53d7b5a7e
MartinSterckx <61700408+MartinSterckx@users.noreply.github.com>
2020-05-07 10:19:25 +0200

    Update NousContacter.php

--- views/NousContacter.php
+++ views/NousContacter.php
@@ -17,32 +17,32 @@
         </div>
         <div class="mail">
             <a href="envoiMail.php">
-                <img src=Image/"mail.png" class="contacter_image" style="width: 140px; height: 100px;"></a>
+                <img src="Image/mail.png" class="contacter_image" style="width: 140px; height: 100px;"></a>
             <p>
                 <font size="+1">infinitemeasures.society@gmail.com</font>
             </p>
         </div>
         <div class="ReseauSociaux">
             <div id="contacter_block">
-                <a href="https://www.instagram.com/infinite_measures_society/"> <img src=Image/"logoInsta.png" class="contacter_image"></a>
+                <a href="https://www.instagram.com/infinite_measures_society/"> <img src="Image/logoInsta.png" class="contacter_image"></a>
                 <p>
                     <font size="+1">Instagram</font>
                 </p>
             </div>
             <div id="contacter_block">
-                <a href="https://twitter.com/InfiniteMeasur2"><img src=Image/"logoTwitter.png" class="contacter_image"></a>
+                <a href="https://twitter.com/InfiniteMeasur2"><img src="Image/logoTwitter.png" class="contacter_image"></a>
                 <p>
                     <font size="+1">Twitter</font>
                 </p>
             </div>
             <div id="contacter_block">
-                <a href="https://www.linkedin.com/in/infinite-measures-8454771a5/"><img src="logoLinked.png" class="contacter_image"></a>
+                <a href="https://www.linkedin.com/in/infinite-measures-8454771a5/"><img src="Image/logoLinked.png" class="contacter_image"></a>
                 <p>
                     <font size="+1">Linkedin</font>
                 </p>
             </div>
             <div id="contacter_block">
-                <a href="https://www.facebook.com/Infinite-Measures-107016680954018/"><img src="logoFacebook.png" class="contacter_image"></a>
+                <a href="https://www.facebook.com/Infinite-Measures-107016680954018/"><img src="Image/logoFacebook.png" class="contacter_image"></a>
                 <p>
                     <font size="+1">Facebook</font>
                 </p>

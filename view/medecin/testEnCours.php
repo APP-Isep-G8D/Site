405a481c76abc17e89d533d9347877dd12cbde03
RENAN <renan@redinateur.home>
2020-05-14 19:47:12 +0200

    anim

--- view/medecin/testEnCours.php
+++ view/medecin/testEnCours.php
@@ -3,33 +3,28 @@
 
 <div id="conteneurTest2">
   <div id="conteneurTest">
-  <div id="chargement">
-    Récupération des valeurs
-  </div>
+    <div id="chargement">
+      Récupération des valeurs
+    </div>
 
-  <div style="margin-left: 20%;margin-right: 20%;">
-    <div class="bordure">
-      <div id="myBar" class="completion">0%</div>
+    <div style="margin-left: 20%;margin-right: 20%;">
+    <br>
+      <div class="bordure">
+        <div id="myBar" class="completion">0%</div>
+      </div>
+    </div>
+
+    <div id="recommencer_test">
+      <a onclick="location.reload()">
+        <img src="public/images/restart_button.png">
+      </a>
+    </div>
+
+    <div id="retour">
+      <a style="font-size:large" href="<?php echo $_SERVER['HTTP_REFERER']; ?>" id="erreur_boutonr">Retour</a>
+
     </div>
   </div>
-
-  <div id="recommencer_test">
-    <br>
-    <br>
-    <br>
-    <a onclick="location.reload()" id="erreur_bouton">
-      Recommencer
-    </a>
-  </div>
-
-  <div id="retour">
-    <br>
-    <br>
-    <br>
-    <a style="font-size:large" href="<?php echo $_SERVER['HTTP_REFERER']; ?>" id="erreur_boutonr">Retour</a>
-
-  </div>
-</div>
 </div>
 
 

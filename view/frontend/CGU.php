0fdbc07085e8c8979457644f6b09dc6ca4b3e4dc
DarkObscuro <56973532+DarkObscuro@users.noreply.github.com>
2020-05-15 12:13:52 +0200

    test

--- view/frontend/CGU.php
+++ view/frontend/CGU.php
@@ -12,7 +12,7 @@
 			<br>
 			Les capteurs ne peuvent pas être utilisés en dehors de centre agréées dont la liste est disponible sur le site internet.
 			<br>
-			Les techniques et prises de mesures sont réalisées par des professionnels surentrainés. En conséquence, il ne faut pas tenter de reproduire ou d'imiter le contenue de ce projet. Ne faites pas ça chez vous.
+			Les techniques et prises de mesures sont réalisées par des professionnels surentrainés. En conséquence, il ne faut pas tenter de reproduire ou d'imiter le contenu de ce projet. Ne faites pas ça chez vous.
 			<br>
 			Le capteurator mais les résultats sont bons.
 		</div>

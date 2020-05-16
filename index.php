4753727836af0b3e916c348b040ed1a7f94fd5ef
RENAN <renan@redinateur.home>
2020-05-16 14:41:11 +0200

    switch

--- index.php
+++ index.php
@@ -6,68 +6,98 @@ require('controller/admin.php');
 require('controller/medecin.php');
 
 if (isset($_GET['action'])) {
-    if ($_GET['action'] == 'main') {
-        main();
-    } elseif ($_GET['action'] == 'faq') {
-        faq();
-    } elseif ($_GET['action'] == 'contactus') {
-        contactus();
-    } elseif ($_GET['action'] == 'Professionnel') {
-        professionnel();
-    } elseif ($_GET['action'] == 'SAV') {
-        sav();
-    } elseif ($_GET['action'] == 'CGU') {
-        cgu();
-    } elseif ($_GET['action'] == 'envoiMail') {
-        envoiMail();
-    } elseif ($_GET['action'] == 'patienter') {
-        patienter();
-    } elseif ($_GET['action'] == 'logout') {
-        logout();
-    } elseif ($_GET['action'] == 'login') {
-        login();
-    } elseif ($_GET['action'] == 'redirect') {
-        redirect();
-    } elseif ($_GET['action'] == 'admin') {
-        admin();
-    } elseif ($_GET['action'] == 'centre') {
-        centre();
-    } elseif ($_GET['action'] == 'ajoutBoitier') {
-        ajoutBoitier();
-    } elseif ($_GET['action'] == 'verifDelBoitier') {
-        verifDelBoitier();
-    } elseif ($_GET['action'] == 'medSupp') {
-        verifDelMedecin();
-    } elseif ($_GET['action'] == 'ajoutMedecin') {
-        ajoutMedecin();
-    } elseif ($_GET['action'] == 'verifDelCentre') {
-        verifDelCentre();
-    } elseif ($_GET['action'] == 'ajoutCentre') {
-        ajoutCentre();
-    } elseif ($_GET['action'] == 'delCentre') {
-        if (isset($_GET['id']) && centreExist($_GET['id'])) {
-            delCentre($_GET["id"]);
-        } else {
+    switch ($_GET['action']) {
+        case ('main'):
             main();
-        }
-    } elseif ($_GET['action'] == 'removeBoitier') {
-        removeBoitier();
-    } elseif ($_GET['action'] == 'removeMedecin') {
-        removeMedecin();
-    } elseif ($_GET['action'] == 'medecin') {
-        medecin();
-    } elseif ($_GET['action'] == 'profilPatient') {
-        profilPatient();
-    } elseif ($_GET['action'] == 'test') {
-        test();
-    } elseif ($_GET['action'] == 'newTest') {
-        newTest();
-    } elseif ($_GET['action'] == 'ajoutPatient') {
-        ajoutPatient();
-    } elseif ($_GET['action'] == 'testEnCours') {
-        testEnCours();
-    } else {
-        error();
+            break;
+        case ('faq'):
+            faq();
+            break;
+        case ('contactus'):
+            contactus();
+            break;
+        case ('Professionnel'):
+            professionnel();
+            break;
+        case ('SAV'):
+            sav();
+            break;
+        case ('CGU'):
+            cgu();
+            break;
+        case ('envoiMail'):
+            envoiMail();
+            break;
+        case ('patienter'):
+            patienter();
+            break;
+        case ('logout'):
+            logout();
+            break;
+        case ('login'):
+            login();
+            break;
+        case ('redirect'):
+            redirect();
+            break;
+        case ('admin'):
+            admin();
+            break;
+        case ('centre'):
+            centre();
+            break;
+        case ('ajoutBoitier'):
+            ajoutBoitier();
+            break;
+        case ('verifDelBoitier'):
+            verifDelBoitier();
+            break;
+        case ('medSupp'):
+            verifDelMedecin();
+            break;
+        case ('ajoutMedecin'):
+            ajoutMedecin();
+            break;
+        case ('verifDelCentre'):
+            verifDelCentre();
+            break;
+        case ('ajoutCentre'):
+            ajoutCentre();
+            break;
+        case ('delCentre'):
+            if (isset($_GET['id']) && centreExist($_GET['id'])) {
+                delCentre($_GET["id"]);
+            } else {
+                main();
+            }
+            break;
+        case ('removeBoitier'):
+            removeBoitier();
+            break;
+        case ('removeMedecin'):
+            removeMedecin();
+            break;
+        case ('medecin'):
+            medecin();
+            break;
+        case ('profilPatient'):
+            profilPatient();
+            break;
+        case ('test'):
+            test();
+            break;
+        case ('newTest'):
+            newTest();
+            break;
+        case ('ajoutPatient'):
+            ajoutPatient();
+            break;
+        case ('testEnCours'):
+            testEnCours();
+            break;
+        default:
+            error();
+            break;
     }
 } else {
     main();

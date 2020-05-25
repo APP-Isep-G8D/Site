d29ff943708aded05544b851e103970dbd0955f2
DarkObscuro <56973532+DarkObscuro@users.noreply.github.com>
2020-05-25 15:39:10 +0200

    Update test.php

--- view/medecin/test.php
+++ view/medecin/test.php
@@ -14,6 +14,14 @@ $dataPoints = array(
     array("y" => $mesures["reactivite"], "label" => "Réactivité")
 
 );
+
+$dataPoints2 = array(
+    array("y" => 50, "label" => "Moyenne fréquence Cardiaque"),
+    array("y" => 40, "label" => "Température"),
+    array("y" => 65, "label" => "Cohérence audio"),
+    array("y" => 70, "label" => "Réactivité")
+);
+
 $nom = $prenom = $adresse = $numeroSecu = "OUI";
 ?>
 
@@ -37,21 +45,33 @@ $nom = $prenom = $adresse = $numeroSecu = "OUI";
     window.onload = function() {
         var chart = new CanvasJS.Chart("chartContainer", {
             animationEnabled: true,
-            theme: "light2",
+            theme: "dark2",
             title: {
                 text: "Test du <?php echo   $test["date"] ?>"
             },
             axisY: {
                 title: " Réussite (%)"
             },
-            data: [{
-                type: "column",
-
-                dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
-            }]
+            data: [
+                {
+                    type: "column",
+                    color: "#6495ED",
+                    dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
+                },
+	            {
+                    type: "splineArea",
+                    fillOpacity: .3, 
+                    color: "orange",
+		            name: "Moyenne",
+		            showInLegend: true,
+		            dataPoints: <?php echo json_encode($dataPoints2, JSON_NUMERIC_CHECK); ?>
+	}]
         });
+    
         chart.render();
     }
+
+
 </script>
 
 <?php $content = ob_get_clean(); ?>
